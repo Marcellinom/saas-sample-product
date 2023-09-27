562ebc9e9cb6d7d08f9deb15fedd5bbb28bb15ab
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2023-09-27 15:22:40 +0700

    fix: wrong role being serialized

--- pkg/auth/services/login.go
+++ pkg/auth/services/login.go
@@ -12,7 +12,7 @@ import (
 func Login(ctx *gin.Context, u *contracts.User) error {
 	sess := session.Default(ctx)
 	sess.Set("user.id", strings.ToLower(u.Id()))
-	sess.Set("user.active_role", strings.ToLower(u.Id()))
+	sess.Set("user.active_role", u.ActiveRole())
 	rolesJson, err := json.Marshal(u.Roles())
 	if err != nil {
 		return err
