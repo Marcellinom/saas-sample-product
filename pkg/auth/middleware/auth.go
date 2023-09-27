51407ec985e0c002a7284fa893de03c5a734609d
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2023-09-28 01:00:14 +0700

    chore: unserialize role & active role

--- pkg/auth/middleware/auth.go
+++ pkg/auth/middleware/auth.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -19,28 +20,43 @@ func Auth() gin.HandlerFunc {
 			return
 		}
 		// TODO: Unserialize roles
-		// activeRoleIf, ok := sess.Get("user.active_role")
-		// if !ok {
-		// 	ctx.AbortWithStatusJSON(http.StatusUnauthorized, common.UnauthorizedResponse)
-		// 	return
-		// }
-		// rolesJsonIf, ok := sess.Get("user.roles")
-		// if !ok {
-		// 	ctx.AbortWithStatusJSON(http.StatusUnauthorized, common.UnauthorizedResponse)
-		// 	return
-		// }
+		activeRoleIf, ok := sess.Get("user.active_role")
+		if !ok {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, common.UnauthorizedResponse)
+			return
+		}
+		rolesJsonIf, ok := sess.Get("user.roles")
+		if !ok {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, common.UnauthorizedResponse)
+			return
+		}
+		activeRole, ok := activeRoleIf.(string)
+		if !ok {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, common.UnauthorizedResponse)
+			return
+		}
+		rolesJson, ok := rolesJsonIf.(string)
+		if !ok {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, common.UnauthorizedResponse)
+			return
+		}
+		var roles []contracts.Role
+		err := json.Unmarshal([]byte(rolesJson), &roles)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, common.UnauthorizedResponse)
+		}
+
 		id, ok := idIf.(string)
 		if !ok {
 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, common.UnauthorizedResponse)
 			return
 		}
-		// activeRole, ok := activeRoleIf.(string)
-		// if !ok {
-		// 	ctx.AbortWithStatusJSON(http.StatusUnauthorized, common.UnauthorizedResponse)
-		// 	return
-		// }
 
 		u := contracts.NewUser(id)
+		for _, role := range roles {
+			u.AddRole(role.Name, role.Permissions, role.IsDefault)
+		}
+		u.SetActiveRole(activeRole)
 
 		ctx.Set(utils.UserKey, u)
 		ctx.Next()
