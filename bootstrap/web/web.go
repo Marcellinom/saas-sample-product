d75959a4f0e368e8344bc35c501202a37695f77e
awwabi <awwabi.labba@gmail.com>
2023-10-05 09:22:41 +0700

    add open api branch

--- bootstrap/web/web.go
+++ bootstrap/web/web.go
@@ -1,6 +1,9 @@
 package web
 
 import (
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"its.ac.id/base-go/docs"
 	"net/http"
 	"os"
 	"reflect"
@@ -117,6 +120,15 @@ func (g *GinServer) buildRouter() *gin.Engine {
 			"data":    nil,
 		})
 	})
+
+	// programmatically set swagger info
+	docs.SwaggerInfo.Title = "Swagger Example API"
+	docs.SwaggerInfo.Description = "This is a sample server Petstore server."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "petstore.swagger.io"
+	docs.SwaggerInfo.BasePath = "/v2"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+	g.engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	HookBuildRouter.Dispatch(g.engine)
 	return g.engine
