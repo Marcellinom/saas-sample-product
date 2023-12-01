bec1f436f9ec5797390fe14823076d80a2db3be8
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2023-12-01 12:57:45 +0700

    chore: global middleware

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"bitbucket.org/dptsi/base-go-libraries/web"
-	"bitbucket.org/dptsi/base-go-libraries/web/middleware"
+	webMiddleware "bitbucket.org/dptsi/base-go-libraries/web/middleware"
 	"github.com/joho/godotenv"
 	"github.com/samber/do"
+	"its.ac.id/base-go/bootstrap/event"
+	"its.ac.id/base-go/bootstrap/middleware"
 	// Services
 )
 
@@ -33,15 +36,22 @@ func main() {
 	i := do.DefaultInjector
 	createObjects(i)
 	log.Println("Setting up web server...")
-	server, err := web.SetupServer()
+	server, err := web.SetupServer(web.Config{
+		IsDebugMode: os.Getenv("APP_DEBUG") == "true",
+		Environment: os.Getenv("APP_ENV"),
+	})
+	server.Use(do.MustInvoke[*middleware.MiddlewareGroup](i).GlobalMiddleware()...)
 	if err != nil {
 		log.Panic(err)
 	}
 	log.Println("Web server successfully set up!")
 
-	// log.Println("Setting up event hook...")
-	// eventHook := event.SetupEventHook()
-	// log.Println("Event hook successfully set up!")
+	log.Println("Setting up event hook...")
+	eventHook := event.SetupEventHook()
+	log.Println("Event hook successfully set up!")
+	do.Provide[*event.EventHook](i, func(i *do.Injector) (*event.EventHook, error) {
+		return eventHook, nil
+	})
 
 	// log.Println("Registering modules...")
 	// modules.RegisterModules(server.Engine(), eventHook)
@@ -54,8 +64,8 @@ func main() {
 }
 
 func createObjects(i *do.Injector) {
-	do.ProvideNamed[*middleware.HandleCors](i, "HandleCorsMiddleware", func(i *do.Injector) (*middleware.HandleCors, error) {
-		return &middleware.HandleCors{
+	do.ProvideNamed[*webMiddleware.HandleCors](i, "HandleCorsMiddleware", func(i *do.Injector) (*webMiddleware.HandleCors, error) {
+		return &webMiddleware.HandleCors{
 			AllowedOrigins:   []string{"http://localhost:3000"},
 			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 			AllowedHeaders:   []string{"content-type", "x-csrf-token"},
@@ -64,5 +74,7 @@ func createObjects(i *do.Injector) {
 			MaxAge:           0,
 		}, nil
 	})
-
+	do.Provide[*middleware.MiddlewareGroup](i, func(i *do.Injector) (*middleware.MiddlewareGroup, error) {
+		return middleware.NewMiddlewareGroup(i), nil
+	})
 }
