5d687a34d0eddf303c9d9fdad4fef4a2002d79ee
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2024-01-04 13:52:37 +0700

    chore: add auth using git submodule

--- providers/module.go
+++ providers/module.go
@@ -2,7 +2,9 @@ package providers
 
 import (
 	"bitbucket.org/dptsi/its-go/contracts"
+	"its.ac.id/base-go/modules/auth"
 )
 
 func registerModules(service contracts.ModuleService) {
+	service.Register("auth", auth.SetupModule)
 }
